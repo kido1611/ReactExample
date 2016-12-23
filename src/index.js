5d2bdbcfbd368b8b66dcbb5a329159894fa5dad3
kido1611 <aci@hotmail.co.id>
2016-12-24 03:33:07 +0700

    Learn 2.2, Embedding Expression in JSX

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const element = <h1>Hello World!!</h1>
+function formatName(user){
+	return user.firstName+' '+user.lastName;
+}
+
+const user = {
+	firstName: 'Muhammad',
+	lastName: 'Abdusy Syukur'
+};
+
+const element = (
+	<h1>Hello, {formatName(user)}</h1>
+);
 
 ReactDOM.render(
   element,
