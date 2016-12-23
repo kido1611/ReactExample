d32ae698190e93c1b9d4079808d513a6957148fa
kido1611 <aci@hotmail.co.id>
2016-12-24 05:40:14 +0700

    Learn 2.6, JSX Represent Objeects

--- src/index.js
+++ src/index.js
@@ -3,33 +3,10 @@ import ReactDOM from "react-dom";
 import logo from './logo.svg';
 import './index.css';
 
-const user = {
-	firstName: 'Muhammad',
-	lastName: 'Abdusy Syukur'
-};
-
-function formatName(user){
-	return user.firstName+' '+user.lastName;
-}
-
-function getGreeting(user){
-	if(user){
-		return <h1>Hello, {formatName(user)}</h1>;
-	}
-	return <h1>Hello, guest</h1>
-}
-
-
-// <img src={logo} className="app-image"/> has no children
-// so it has closed tag />
-
-const element = (
-	<div tabIndent="0">
-		<img src={logo} className="app-image"/>
-		{getGreeting(user)}
-		<br/>
-		{getGreeting()}
-	</div>
+const element = React.createElement(
+	'h1', 
+	{className: 'greeting'}, 
+	'Hello, world'
 );
 
 ReactDOM.render(
